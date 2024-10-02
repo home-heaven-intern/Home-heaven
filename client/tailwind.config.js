585cdaf36b05493864ed5c3afbd5e34d771e0352
chandan adlak <chandanadlak4321@gmail.com>
2024-10-02 16:17:12 +0530

    initial navbar

---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./main.jsx", // Include the main HTML file
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
